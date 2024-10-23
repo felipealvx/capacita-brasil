3139f6270a4970662b9c8099af6b4aec4486ba13
Felipe Alves <57331619+felipealvx@users.noreply.github.com>
2024-10-22 22:44:32 -0300

    update while loop

--- calculator/main.swift
+++ calculator/main.swift
@@ -1,22 +1,33 @@
 print("---------CALCULADORA----------")
-print("Digite o primeiro número:")
+print ("Digite 1 para entrar na calculadora")
+if let optional = readLine(), let optionalInt = Int(optional) {
+  while optionalInt == 1 {
+    print("Digite o primeiro número:")
 
-if let num1 = readLine(), let num1Int = Int(num1){
-  print("Digite o segundo número:")
-  if let num2 = readLine(), let num2Int = Int(num2){
-    print("Escolha a operação: \n + ----- soma \n - ----- subtração \n * ----- multiplicação \n / ----- divisão")
-    if let operacao = readLine(){
-      switch operacao {
-      case "+":
-        print ("Resultado: \(num1Int) + \(num2Int) =", num1Int + num2Int)
-      case "-":
-        print ("Resultado: \(num1Int) - \(num2Int) =", num1Int - num2Int)
-      case "*":
-        print ("Resultado: \(num1Int) * \(num2Int) =", num1Int * num2Int)
-      case "/":
-        print("Resultado: \(num1Int) / \(num2Int) =", num1Int / num2Int)
-      default:
-        print("Operação inválida")
+    if let num1 = readLine(), let num1Int = Int(num1){
+      print("Digite o segundo número:")
+      if let num2 = readLine(), let num2Int = Int(num2){
+        print("Escolha a operação: \n + ----- soma \n - ----- subtração \n * ----- multiplicação \n / ----- divisão")
+        if let operacao = readLine(){
+          switch operacao {
+          case "+":
+            print ("Resultado: \(num1Int) + \(num2Int) =", num1Int + num2Int)
+          case "-":
+            print ("Resultado: \(num1Int) - \(num2Int) =", num1Int - num2Int)
+          case "*":
+            print ("Resultado: \(num1Int) * \(num2Int) =", num1Int * num2Int)
+          case "/":
+            print("Resultado: \(num1Int) / \(num2Int) =", num1Int / num2Int)
+          default:
+            print("Operação inválida")
+          }
+        }
+      }
+    }
+    print("Deseja sair da calculadora? \n y ----- sim \n n ----- não")
+    if let sair = readLine(){
+      if sair.uppercased() == "Y"{
+        break
       }
     }
   }
